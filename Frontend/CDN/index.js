529eb6c3baeccc42347f5d475278a512626134d3
DevanshParashar165 <parashardevansh165@gmail.com>
2025-10-27 13:06:19 +0530

    assignment

---
+++ Frontend/CDN/index.js
@@ -0,0 +1,18 @@
+const data = 'data'
+const reactElement =<div><div>This is div content</div></div>
+const heading = <div>
+    <div>
+        <span>Span1</span>
+        <span>Span2</span>
+    </div>
+    <div>
+        <span>Span1</span>
+        <span>Span2</span>
+    </div>
+    <div>
+        <span>Span1</span>
+        <span>Span2</span>
+    </div>
+</div>
+
+const root = React.createElement('')
