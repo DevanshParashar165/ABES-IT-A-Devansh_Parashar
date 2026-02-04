41b2a5449988790a14da6771dc52f388323e39de
DevanshParashar165 <parashardevansh165@gmail.com>
2026-02-04 10:10:41 +0530

    Node server image

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -3,6 +3,24 @@ import http from "http"
 const app = http.createServer((req,res)=>{
     // console.log('Server started.....')
     // res.end('This is my server')
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const url = req.url
+    if(req.url === '/'){
+        res.end(`<h2>ABES Engineering college</h2>
+            <img src='https://img.freepik.com/free-photo/3d-cartoon-style-character_23-2151034019.jpg?semt=ais_hybrid&w=740&q=80' height = '100px'/>`);
+    }
+    else if(req.url === '/about'){
+        res.end('This is about page');
+    }
+    else if(req.url === '/contact'){
+        res.end('Phone no:- 2345xxxxxx');
+    } 
+    else if(req.url === '/class'){
+        res.end('IT-A');
+    }   
+    else{
+        res.end('404 page not found')
+    }
 })
 
 app.listen(8000,(req,res)=>{
